12049ca5c47aa35d01837cdfe0eee9c1115d0b2e
Evandrosc <evandromdsc@gmail.com>
2023-08-16 01:26:33 -0300

    otimização img

--- src/cardapio.js
+++ src/cardapio.js
@@ -3,56 +3,56 @@ export const cardapio = [
     codigo: 'cafe', 
     descricao: 'Café', 
     valor: 'R$ 3,00', 
-    img: 'img/cafeImg.jpg',
+    img: 'img/cafeImg.webp',
     extra: false
   },
   { 
     codigo: 'chantily', 
     descricao: 'Chantily (extra do Café)', 
     valor: 'R$ 1,50', 
-    img: 'img/chantilyImg.jpg',
+    img: 'img/chantilyImg.webp',
     extra: true
   },
   { 
     codigo: 'suco', 
     descricao: 'Suco Natural', 
     valor: 'R$ 6,20', 
-    img: 'img/sucoImg.jpg',
+    img: 'img/sucoImg.webp',
     extra: false
   },
   { 
     codigo: 'sanduiche', 
     descricao: 'Sanduíche', 
     valor: 'R$ 6,50', 
-    img: 'img/sanduicheImg.jpg',
+    img: 'img/sanduicheImg.webp',
     extra: false
   },
   { 
     codigo: 'queijo', 
     descricao: 'Queijo (extra do Sanduíche)', 
     valor: 'R$ 2,00', 
-    img: 'img/queijoImg.jpg',
+    img: 'img/queijoImg.webp',
     extra: true
   },
   { 
     codigo: 'salgado', 
     descricao: 'Salgado', 
     valor: 'R$ 7,25', 
-    img: 'img/salgadoImg.jpg',
+    img: 'img/salgadoImg.webp',
     extra: false
   },
   { 
     codigo: 'combo1', 
     descricao: '1 Suco e 1 Sanduíche', 
     valor: 'R$ 9,50', 
-    img: 'img/combo1Img.jpg',
+    img: 'img/combo1Img.webp',
     extra: false
   },
   { 
     codigo: 'combo2', 
     descricao: '1 Café e 1 Sanduíche', 
     valor: 'R$ 7,50', 
-    img: 'img/combo2.jpg',
+    img: 'img/combo2.webp',
     extra: false
   }
 ];
