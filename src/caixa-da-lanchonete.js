3c05cb7cb9b0c8c001212dc405fae6b82ece12e9
Evandrosc <evandromdsc@gmail.com>
2023-08-16 03:27:58 -0300

    refatoração CaixaDaLanchonete

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -2,66 +2,75 @@ import { cardapio } from './cardapio.js';
 import { formaDePagamento } from './forma-pagamento.js';
 
 class CaixaDaLanchonete {
-    calcularValorDaCompra(metodoDePagamento, itens) {
-        
-        if (itens.length === 0) {
-            return 'Não há itens no carrinho de compra!';
-        }
+  constructor() {
+    this.precoDosItens = this.calcularPrecosDosItens();
+  }
 
-        const pagamentoValido = formaDePagamento.includes(metodoDePagamento);
-
-        if (!pagamentoValido) {
-            return 'Forma de pagamento inválida!';
-        }
-
-        const precosDosItens = {};
-        for (const item of cardapio) {
-            precosDosItens[item.codigo] = parseFloat(item.valor.replace('R$', '').replace(',', '.'));
-        }
-
-        let total = 0;
-        let sanduicheComprado = false;
-        let cafeComprado = false
-        let itensExtra = [];
-
-        for (const item of itens) {
-            const [produto, quantidade] = item.split(',');
-
-            if (!precosDosItens[produto]) {
-                return 'Item inválido!';
-            }
-
-            if (isNaN(quantidade) || quantidade <= 0) {
-                return 'Quantidade inválida!';
-            }
-
-            if (produto === 'sanduiche') {
-                sanduicheComprado = true;
-            } else if (produto === 'cafe') {
-                cafeComprado = true;
-            } else if (produto !== 'suco') {
-                itensExtra.push(produto);
-            }            
-            
-
-            total += precosDosItens[produto] * parseInt(quantidade, 10);
-        }
-
-        if (metodoDePagamento === 'dinheiro') {
-            total *= 0.95;
-        } else if (metodoDePagamento === 'debito') {
-            total *= 1;
-        } else if (metodoDePagamento === 'credito') {
-            total *= 1.03;
-        }
-
-        if ((itensExtra.includes('chantily') && !cafeComprado) ||
-            (itensExtra.includes('queijo') && !sanduicheComprado)) {
-            return 'Item extra não pode ser pedido sem o principal';
-        }
-
-        return `R$ ${total.toFixed(2).replace('.', ',')}`;
+  calcularPrecosDosItens() {
+    const precosDosItens = {};
+    for (const item of cardapio) {
+      precosDosItens[item.codigo] = parseFloat(item.valor.replace('R$', '').replace(',', '.'));
     }
+    return precosDosItens;
+  }
+
+  validarItem(item, quantidade) {
+    if (!this.precoDosItens[item]) {
+      throw new Error('Item inválido!');
+    }
+
+    if (isNaN(quantidade) || quantidade <= 0) {
+      throw new Error('Quantidade inválida!');
+    }
+  }
+
+  calcularValorDaCompra(metodoDePagamento, itens) {
+    if (itens.length === 0) {
+      return 'Não há itens no carrinho de compra!';
+    }
+
+    if (!formaDePagamento.includes(metodoDePagamento)) {
+      return 'Forma de pagamento inválida!';
+    }
+
+    let total = 0;
+    let sanduicheComprado = false;
+    let cafeComprado = false;
+    const itensAdicionais = [];
+
+    for (const item of itens) {
+      const [produto, quantidade] = item.split(',');
+
+      try {
+        this.validarItem(produto, quantidade);
+      } catch (error) {
+        return error.message;
+      }
+
+      if (produto === 'sanduiche') {
+        sanduicheComprado = true;
+      } else if (produto === 'cafe') {
+        cafeComprado = true;
+      } else if (produto !== 'suco') {
+        itensAdicionais.push(produto);
+      }
+
+      total += this.precoDosItens[produto] * parseInt(quantidade, 10);
+    }
+
+    if (metodoDePagamento === formaDePagamento[0]) {
+      total *= 0.95;
+    } else if (metodoDePagamento !== formaDePagamento[1]) {
+      total *= 1.03;
+    }
+
+    if ((itensAdicionais.includes('chantily') && !cafeComprado) ||
+      (itensAdicionais.includes('queijo') && !sanduicheComprado)) {
+      return 'Item extra não pode ser pedido sem o principal';
+    }
+
+    return `R$ ${total.toFixed(2).replace('.', ',')}`;
+  }
 }
 
 export { CaixaDaLanchonete };
